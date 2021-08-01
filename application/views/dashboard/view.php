2549fa60d9991bf78d3f6dfd91cc721ad022ae6d
Aziz Nurfalah <aziz.nurfalah@bukalapak.com>
2021-08-01 10:53:56 +0700

    fix image path

--- application/views/dashboard/view.php
+++ application/views/dashboard/view.php
@@ -121,7 +121,7 @@
                         <div class="elementor-widget-wrap elementor-element-populated">
                             <div class="elementor-element elementor-element-115f25ad elementor-widget elementor-widget-image" data-id="115f25ad" data-element_type="widget" data-widget_type="image.default">
                                 <div class="elementor-widget-container">
-                                    <img width="786" height="786" src="<?php echo base_url(); ?>assets/image/main/dita.JPG" class="attachment-full size-full" alt="" loading="lazy">
+                                    <img width="786" height="786" src="<?php echo base_url(); ?>assets/image/main/dita.jpg" class="attachment-full size-full" alt="" loading="lazy">
                                 </div>
                             </div>
                             <div class="elementor-element elementor-element-794e3b1e animated-slow elementor-headline--style-highlight elementor-invisible elementor-widget elementor-widget-animated-headline" data-id="794e3b1e" data-element_type="widget" data-settings="{&quot;marker&quot;:&quot;underline&quot;,&quot;highlighted_text&quot;:&quot;Wulan&quot;,&quot;_animation&quot;:&quot;fadeIn&quot;,&quot;headline_style&quot;:&quot;highlight&quot;,&quot;loop&quot;:&quot;yes&quot;,&quot;highlight_animation_duration&quot;:1200,&quot;highlight_iteration_delay&quot;:8000}" data-widget_type="animated-headline.default">
@@ -160,7 +160,7 @@
                         <div class="elementor-widget-wrap elementor-element-populated">
                             <div class="elementor-element elementor-element-2a71c366 elementor-widget elementor-widget-image" data-id="2a71c366" data-element_type="widget" data-widget_type="image.default">
                                 <div class="elementor-widget-container">
-                                    <img width="835" height="836" src="<?php echo base_url(); ?>assets/image/main/aziz.JPG" class="attachment-full size-full" alt="" loading="lazy">
+                                    <img width="835" height="836" src="<?php echo base_url(); ?>assets/image/main/aziz.jpg" class="attachment-full size-full" alt="" loading="lazy">
                                 </div>
                             </div>
                             <div class="elementor-element elementor-element-24ccc768 animated-slow elementor-headline--style-highlight elementor-invisible elementor-widget elementor-widget-animated-headline" data-id="24ccc768" data-element_type="widget" data-settings="{&quot;marker&quot;:&quot;underline&quot;,&quot;highlighted_text&quot;:&quot;Chandra&quot;,&quot;_animation&quot;:&quot;fadeIn&quot;,&quot;headline_style&quot;:&quot;highlight&quot;,&quot;loop&quot;:&quot;yes&quot;,&quot;highlight_animation_duration&quot;:1200,&quot;highlight_iteration_delay&quot;:8000}" data-widget_type="animated-headline.default">
